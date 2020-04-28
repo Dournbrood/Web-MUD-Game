487ba5859da8bcb5fcfcd8ab9038e8a46c206e42
imxande <alerodriguezlarosa@gmail.com>
2020-04-28 17:23:02 -0400

    fixed bugs and added context API

--- build-week-mud/src/App.js
+++ build-week-mud/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,21 +8,29 @@ import NavBar from './Components/NavBar'
 import Login from './Components/Login'
 import SignUp from './Components/SignUp'
 import { ThemeProvider, theme } from '@chakra-ui/core';
+import Context from './context/context';
 
 function App() {
+  // This holds the state to pass via provider we probably need it whenever we check for those credentials to login 
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
   return (
-    <ThemeProvider theme={theme}>
-        <div> MUD Front End </div>
-        <Router>
-            <NavBar />
-            <Route path='/login'>
-                <Login />
-            </Route>
-            <Route path='/register'>
-                <SignUp />
-            </Route>
-        </Router>
-    </ThemeProvider>
+    <Context.Provider value = {{credentials, setCredentials}}>
+      <ThemeProvider theme={theme}>
+          <Router>
+              <NavBar />
+              <Route path='/login'>
+                  <Login />
+              </Route>
+              <Route path='/register'>
+                  <SignUp />
+              </Route>
+          </Router>
+      </ThemeProvider>
+    </Context.Provider>
   );
 }
 
