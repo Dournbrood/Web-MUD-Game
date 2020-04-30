3565f2c8b23940880c9f5c91cedce70c81852427
Taylor Cool <taycool@gmail.com>
2020-04-29 18:31:01 -0700

    getting data from backend, not drawing map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { scaleLinear } from "d3-scale";
+import { axiosWithAuth } from "../Utils/axiosWithAuth";
 
 var nodes = [
-  { x: 40, y: 60 },
-  { x: 50, y: 60, active: true },
-  { x: 50, y: 50 },
-  { x: 50, y: 40},
-  { x: 50, y: 70 },
-  { x: 60, y: 60 },
-  { x: 70, y: 60 },
-  { x: 60, y: 70 }
+//   { x: 40, y: 60 },
+//   { x: 50, y: 60, active: true },
+//   { x: 50, y: 50 },
+//   { x: 50, y: 40},
+//   { x: 50, y: 70 },
+//   { x: 60, y: 60 },
+//   { x: 70, y: 60 },
+//   { x: 60, y: 70 }
 ];
 
 var links = [
-  { start: 0, end: 1 },
-  { start: 1, end: 2 },
-  { start: 2, end: 3 },
-  { start: 1, end: 4 },
-  { start: 0, end: 5 },
-  { start: 5, end: 6 },
-  { start: 5, end: 7 },
-  { start: 4, end: 7 }
+//   { start: 0, end: 1 },
+//   { start: 1, end: 2 },
+//   { start: 2, end: 3 },
+//   { start: 1, end: 4 },
+//   { start: 0, end: 5 },
+//   { start: 5, end: 6 },
+//   { start: 5, end: 7 },
+//   { start: 4, end: 7 }
 ];
 
 var mapStyles = { position: "relative" };
@@ -33,6 +34,24 @@ export default function Map({ width, height }) {
   var yScale = scaleLinear()
     .domain([0, 100])
     .range([0, height]);
+
+    axiosWithAuth()
+        .get('adv/map')
+        .then(res => {
+            let node_data = res.data.all_rooms
+            node_data.forEach(element => {
+                const node = {
+                    x: element.x * 10,
+                    y: element.y * 10,
+                    room_id: element.id
+                }
+                nodes.push(node)
+            });
+        })
+        .catch(err => {
+            console.log('error: ', err)
+        })
+        console.log(nodes)
 
   return (
     <div id="map" style={mapStyles}>
@@ -57,9 +76,9 @@ export default function Map({ width, height }) {
             stroke="teal"
           />
         ))}
-        {nodes.map((node, i) => (
+        {nodes.map((node) => (
           <circle
-            key={i}
+            key={node.room_id}
             cx={xScale(node.x)}
             cy={yScale(node.y)}
             r="5"
