fa09dbc936080edb64f0383f042566417a1e422c
Taylor Cool <taycool@gmail.com>
2020-04-30 09:17:41 -0700

    still working on map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { scaleLinear } from "d3-scale";
+// import { scaleLinear } from "d3-scale";
 import { axiosWithAuth } from "../Utils/axiosWithAuth";
 
 var nodes = [
-//   { x: 40, y: 60 },
+//   { x: 40, y: 60, room_id: 9, active: true }
 //   { x: 50, y: 60, active: true },
 //   { x: 50, y: 50 },
 //   { x: 50, y: 40},
@@ -14,7 +14,7 @@ var nodes = [
 ];
 
 var links = [
-//   { start: 0, end: 1 },
+//   { start: 11, end: 12 },
 //   { start: 1, end: 2 },
 //   { start: 2, end: 3 },
 //   { start: 1, end: 4 },
@@ -28,12 +28,12 @@ var mapStyles = { position: "relative" };
 var svgStyles = { position: "absolute", top: 0, left: 0, right: 0, bottom: 0 };
 
 export default function Map({ width, height }) {
-  var xScale = scaleLinear()
-    .domain([0, 100])
-    .range([0, width]);
-  var yScale = scaleLinear()
-    .domain([0, 100])
-    .range([0, height]);
+//   var xScale = scaleLinear()
+//     .domain([0, 100])
+//     .range([0, width]);
+//   var yScale = scaleLinear()
+//     .domain([0, 100])
+//     .range([0, height]);
 
     axiosWithAuth()
         .get('adv/map')
@@ -65,26 +65,26 @@ export default function Map({ width, height }) {
         height={height}
         viewBox={`0 0 ${width} ${height}`}
       >
-        {links.map((link, i) => (
-          <line
-            key={i}
-            x1={xScale(nodes[link.start].x)}
-            x2={xScale(nodes[link.end].x)}
-            y1={yScale(nodes[link.start].y)}
-            y2={yScale(nodes[link.end].y)}
-            strokeWidth={2}
-            stroke="teal"
-          />
-        ))}
-        {nodes.map((node) => (
-          <circle
+        {nodes.map(node => (
+            <circle
             key={node.room_id}
-            cx={xScale(node.x)}
-            cy={yScale(node.y)}
+            cx={node.x}
+            cy={node.y}
             r="5"
             fill={node.active === true ? "yellow" : "teal"}
-          />
+            />
         ))}
+        {/* {links.map((link, i) => (
+            <line
+            key={i}
+            x1={nodes[link.start].x}
+            x2={nodes[link.end].x}
+            y1={nodes[link.start].y}
+            y2={nodes[link.end].y}
+            strokeWidth={2}
+            stroke="teal"
+            />
+        ))} */}
       </svg>
     </div>
   );
